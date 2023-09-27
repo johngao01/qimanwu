035cb951ff0d88ba01e9815f0d13c0ec19f23388
johngao01 <1649868243@qq.com>
2023-09-27 18:53:09 +0800

    重新设计了item

--- qimanwu/items.py
+++ qimanwu/items.py
@@ -12,11 +12,19 @@ class QimanwuItem(scrapy.Item):
     pass
 
 
-class ImageSpiderItem(scrapy.Item):
+class ChapterItem(scrapy.Item):
+    manhua_id = scrapy.Field()
+    chapter_id = scrapy.Field()
+    name = scrapy.Field()
+    url = scrapy.Field()
+
+
+class ContentItem(scrapy.Item):
     # Field类仅是内置字典类（dict）的一个别名，并没有提供额外的方法和属性。
     # 被用来基于类属性的方法来支持item生命语法。
-    url = scrapy.Field()
-    name = scrapy.Field()
-    chapter_name = scrapy.Field()
     manhua_id = scrapy.Field()
     manhua_name = scrapy.Field()
+    chapter_id = scrapy.Field()
+    chapter_name = scrapy.Field()
+    name = scrapy.Field()
+    url = scrapy.Field()
